61672dcbd065ebd16a3e75f8e52f99a287f7bb2a
Jordon Trujillo <jordontrujillo@outlook.com>
2024-12-26 16:24:55 -0600

    Add api and separate client code

--- src/contexts.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-// Default value for createContext() is the contents from a hook, in this case,
-// an array and the setter function for the array
-// https://react.dev/reference/react/createContext#createcontext
-export const CartContext = createContext([[], function () {}]);
